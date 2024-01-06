0e45e23b2565acb99d1e0bf7a7ed8595ab6c8670
Aslam Mohammed Hamed <137844598+aslmhamed@users.noreply.github.com>
2024-01-06 17:19:26 +0200

    Update LeapYear.py

--- DayOne/LeapYear.py
+++ DayOne/LeapYear.py
@@ -1,14 +1,10 @@
 def is_leap(year):
     leap = False
-    
-    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0): # using logilc operetors to check if it's leap or not 
+    # using logilc operetors to check if it's leap or not 
+    if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0): 
         leap = True
     else:
         leap = False
-                
-
-    
-    
     return leap
 
 year = int(input())
